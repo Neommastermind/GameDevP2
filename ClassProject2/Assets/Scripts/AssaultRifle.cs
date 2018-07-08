dbeded11f9133baf6965851bd8ad0f8abf1228ae
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-07-08 00:27:21 -0600

    Part 1 Start-Finish
    Started and finished part 1.

---
+++ ClassProject2/Assets/Scripts/AssaultRifle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssaultRifle : Gun {
+
+	override protected void Update() {
+		base.Update ();
+		//AssaultRfile has automatic fire rate
+		if (Input.GetMouseButton (0) && (Time.time - lastFireTime) > fireRate) {
+			lastFireTime = Time.time;
+			Fire ();
+		}
+	}
+}
