30d8c4467ce17361db848841dfd519909a3e164b
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-07-10 02:21:52 -0600

    Part 2 complete
    Completed part 2.

---
+++ ClassProject2/Assets/Scripts/Missile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	public float speed = 30f;
+	public int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("deathTimer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	IEnumerator deathTimer() {
+		yield return new WaitForSeconds (10);
+		Destroy (gameObject);
+	}
+
+	void OnCollisionEnter(Collision collider) {
+		if(collider.gameObject.GetComponent<Player>() != null
+			&& collider.gameObject.tag == "Player") {
+			collider.gameObject.GetComponent<Player> ().TakeDamage (damage);
+		}
+
+		Destroy (gameObject);
+	}
+}
---
+++ ClassProject2/Assets/Scripts/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	public int type;
+
+	void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.GetComponent<Player>() != null
+			&& collider.gameObject.tag == "Player") {
+
+			collider.gameObject.GetComponent<Player> ().PickUpItem (type);
+			GetComponentInParent<PickUpSpawn> ().PickupWasPickedUp();
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
